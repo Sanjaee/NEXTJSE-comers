6827ff1297a6b98ef9159be108d2f1bc1d6cdbb4
Sanjaee <afrizaahmad18@gmail.com>
2023-11-04 12:52:52 +0700

    url

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -26,7 +26,7 @@ export async function getServerSideProps({
   params: { product: string };
 }) {
   const res = await fetch(
-    `http://localhost:3000/api/product/${params.product}`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/product/${params.product}`
   );
   const response = await res.json();
 
